7e29b3acd8615cb373a3961373234211ff499762
vcooper <vanessa.cooper1@csuglobal.edu>
2024-01-19 18:32:12 -0600

    Final version.

--- SimpleProg1.java
+++ SimpleProg1.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class SimpleProg1 {
     public static void main(String[] args) {
         System.out.println("Hello, player what is your name? ");
+        Scanner playerName = new Scanner(System.in);
+
+        String name = playerName.next();
+        System.out.println("Hello, " + name);
     }
 
 }
